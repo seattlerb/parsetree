75fd8104893f81323a6e5366061b6504b0ea85f4
Ryan Davis <ryand@zenspider.com>
2007-07-02 23:51:18 -0800

    finished unifier for defn/fcall/bmethod/dmethod.
    [git-p4: depot-paths = "//src/ParseTree/dev/": change = 3285]

--- lib/unified_ruby.rb
+++ lib/unified_ruby.rb
@@ -1,5 +1,42 @@
 
 module UnifiedRuby
+#   def rewrite_defs(exp)
+#     exp[0] = :defn
+#     target = exp.delete_at 1
+#     exp[1] = :"#{target}.#{exp[1]}"
+#     exp
+#   end
+
+  # s(:defn, :name, s(:scope, s(:block, s(:args, ...), ...)))
+  # s(:defn, :name, s(:bmethod, s(:masgn, s(:dasgn_curr, :args)), s(:block, ...)))
+  # s(:defn, :name, s(:fbody, s(:bmethod, s(:masgn, s(:dasgn_curr, :params)), s(:block, ...))))
+  # =>
+  # s(:defn, :name, s(:args, ...), s(:scope, s:(block, ...)))
+
+  def rewrite_defn(exp)
+    # remove fbody envelope
+    exp.push *exp.fbody(true).sexp_body if exp.fbody
+
+    # remove dmethod envelope
+    if exp.dmethod then
+      exp.delete :dmethod_added
+      exp.push *exp.dmethod(true).sexp_body
+    end
+
+    # remove bmethod and convert dvars to lvars
+    if exp.bmethod then
+      exp.push s(:args, *exp.bmethod.dasgn_curr(true).sexp_body)
+      exp.push s(:scope, s(:block, *exp.bmethod(true).sexp_body))
+      exp.find_and_replace_all(:dvar, :lvar)
+    end
+
+    # move args up
+    args = exp.scope.block.args(true) rescue nil
+    exp.insert 2, args if args
+
+    exp
+  end
+
   def rewrite_fcall(exp)
     exp[0] = :call
     exp.insert 1, nil
--- test/test_unified_ruby.rb
+++ test/test_unified_ruby.rb
@@ -4,6 +4,7 @@ $TESTING = true
 
 require 'test/unit' if $0 == __FILE__ unless defined? $ZENTEST and $ZENTEST
 require 'test/unit/testcase'
+require 'sexp'
 require 'sexp_processor'
 require 'unified_ruby'
 
@@ -11,13 +12,20 @@ class TestUnifier < SexpProcessor
   include UnifiedRuby
 end
 
+class Array
+  def to_sexp
+    Sexp.from_array self
+  end
+end
+
 # TODO:
 #
 # 1) DONE [vf]call => call
-# 2) defs x -> defn self.x
-# 3) [bd]method/fbody => defn
-# 4) defn scope block args -> defn args scope block
-# 5) rescue cleanup
+# 2) DONE defn scope block args -> defn args scope block
+# 3) DONE [bd]method/fbody => defn
+# 4) rescue cleanup
+# 5) defs x -> defn self.x # ON HOLD
+# 6) ? :block_arg into args list?
 
 class TestUnifiedRuby < Test::Unit::TestCase
   def setup
@@ -25,19 +33,94 @@ class TestUnifiedRuby < Test::Unit::TestCase
     @sp.require_empty = false
   end
 
-  def test_rewrite_vcall
-    insert = s(:vcall, :puts)
-    expect = s(:call, nil, :puts, nil)
-    result = @sp.process insert
+  def doit
+    assert_equal @expect, @sp.process(@insert)
+  end
 
-    assert_equal expect, result
+  def test_rewrite_defn
+    @insert = s(:defn, :x, s(:scope, s(:block, s(:args), s(:nil))))
+    @expect = s(:defn, :x, s(:args), s(:scope, s(:block, s(:nil))))
+
+    doit
+  end
+
+  def test_rewrite_defn_attr
+    @insert = [:defn, :writer=, [:attrset, :@writer]].to_sexp
+    @expect = @insert.deep_clone
+
+    doit
+  end
+
+  def test_rewrite_defn_bmethod
+    @insert = [:defn,
+               :unsplatted,
+               [:bmethod,
+                [:dasgn_curr, :x],
+                [:call, [:dvar, :x], :+, [:array, [:lit, 1]]]]].to_sexp
+    @expect = [:defn,
+               :unsplatted,
+               [:args, :x],
+               [:scope,
+                [:block,
+                 [:call, [:lvar, :x], :+, [:array, [:lit, 1]]]]]].to_sexp
+
+    doit
+  end
+
+  def test_rewrite_defn_dmethod
+    @insert = [:defn,
+               :dmethod_added,
+               [:dmethod,
+                :a_method,
+                [:scope,
+                 [:block,
+                  [:args, :x],
+                  [:call, [:lvar, :x], :+, [:array, [:lit, 1]]]]]]].to_sexp
+    @expect = [:defn,
+               :a_method,
+               [:args, :x],
+               [:scope,
+                [:block,
+                 [:call, [:lvar, :x], :+, [:array, [:lit, 1]]]]]].to_sexp
+
+    doit
+  end
+
+  def test_rewrite_defn_fbody
+    @insert = [:defn, :an_alias,
+               [:fbody,
+                [:scope,
+                 [:block,
+                  [:args, :x],
+                  [:call, [:lvar, :x], :+, [:array, [:lit, 1]]]]]]].to_sexp
+    @expect = s(:defn, :an_alias,
+                s(:args, :x),
+                s(:scope,
+                 s(:block,
+                  s(:call, s(:lvar, :x), :+, s(:array, s(:lit, 1))))))
+
+    doit
+  end
+
+#   def test_rewrite_defs
+#     insert = s(:defs, :puts)
+#     expect = s(:call, nil, :puts, nil)
+#     result = @sp.process insert
+
+#     assert_equal expect, result
+#   end
+
+  def test_rewrite_vcall
+    @insert = s(:vcall, :puts)
+    @expect = s(:call, nil, :puts, nil)
+
+    doit
   end
 
   def test_rewrite_fcall
-    insert = s(:fcall, :puts, s(:array, s(:lit, :blah)))
-    expect = s(:call, nil, :puts, s(:arglist, s(:lit, :blah)))
-    result = @sp.process insert
+    @insert = s(:fcall, :puts, s(:array, s(:lit, :blah)))
+    @expect = s(:call, nil, :puts, s(:arglist, s(:lit, :blah)))
 
-    assert_equal expect, result
+    doit
   end
 end
