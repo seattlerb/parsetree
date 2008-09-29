67be29b65f755c75ada10c54e38447640f065bee
Ryan Davis <ryand@zenspider.com>
2008-09-29 14:59:20 -0800

    Added dstr_interp_empty
    [git-p4: depot-paths = "//src/ParseTree/dev/": change = 4249]

--- test/pt_testcase.rb
+++ test/pt_testcase.rb
@@ -1885,6 +1885,11 @@ class ParseTreeTestCase < Test::Unit::TestCase
             "RawParseTree" => [:dregx, '', [:evstr, [:ivar, :@rakefile]]],
             "ParseTree"    => s(:dregx, '', s(:evstr, s(:ivar, :@rakefile))))
 
+  add_tests("dregx_interp_empty",
+            "Ruby"         => "/#\{}/",
+            "RawParseTree" => [:dregx, '', [:evstr]],
+            "ParseTree"    => s(:dregx, '', s(:evstr)))
+
   add_tests("dregx_n",
             "Ruby"         => '/#{1}/n',
             "RawParseTree" => [:dregx, '', [:evstr, [:lit, 1]], /x/n.options],
